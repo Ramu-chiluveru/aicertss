0a7e120c1504cf510f2bdfbabc2ff21fd9ce9172
Ramu chiluveru <ramuchiluveru64@gmail.com>
2024-05-30 11:54:30 +0530

    Normalize line endings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './components/HomePage/Homepage';
+import ButtonScroller from './components/BtnScroller/ButtonScroller';
+import WhatWeDo from './components/wwd/WhatWeDo';
+import WhyAi from './components/Why/WhyAi';
+import HeroBanner from './components/HomePage/Homepage';
+import Certificate from './components/certificates/certificate';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <HeroBanner/>
+      <ButtonScroller/>
+      <WhatWeDo/>
+      <WhyAi/>
+      <Certificate/>
+      <Footer/>
     </div>
   );
 }
